5e011054befd13414388ea29858db4656082a665
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-02 12:51:58 -0500

    Adds header and footer

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -3,9 +3,11 @@ import Navbar from './Navbar';
 
 export default function Header() {
   return (
-    <header>
-      <Banner />
-      <Navbar />
+    <header className='bg-gradient-to-t from-dod-900 via-dod-500 to-dod-300'>
+      <nav className='max-w-7xl mx-auto'>
+        <Banner />
+        <Navbar />
+      </nav>
     </header>
   );
 }
