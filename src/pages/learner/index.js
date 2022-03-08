1bc20dc46d10165e3cbd5c8e8b33e523526f2922
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-08 15:03:04 -0500

    adds intial page

---
+++ src/pages/learner/index.js
@@ -0,0 +1,9 @@
+import DefaultLayout from '@/components/layouts/DefaultLayout';
+
+export default function LearnerDashboardPage() {
+  return (
+    <DefaultLayout>
+      <h1>Learner Dashboard</h1>
+    </DefaultLayout>
+  );
+}
