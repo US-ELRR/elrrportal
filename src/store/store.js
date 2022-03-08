6d6010ee354d31a3640b05b2aa3ba471e1a797d0
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-08 15:01:52 -0500

    adds store for zustand

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,51 @@
 import create from 'zustand';
 
+const loadUserDataFromStorage = () => {
+  // ssr
+  if (typeof window === 'undefined') {
+    return null;
+  }
+
+  // csr
+  // check if there is user data in local storage
+  const user = localStorage.getItem('user');
+
+  // parse the user data
+  if (!user) return {};
+  return JSON.parse(user);
+};
+
+const setUserDataToStorage = (user) => {
+  // ssr
+  if (typeof window === 'undefined') return;
+
+  // csr
+  // set the user data to local storage
+  localStorage.setItem('user', JSON.stringify(user));
+};
+
+const removeUserDataFromStorage = () => {
+  // ssr
+  if (typeof window === 'undefined') return;
+
+  // csr
+  // remove the user data from local storage
+  localStorage.removeItem('user');
+};
+
 const useStore = create((set) => ({
-  user: {
-    name: '',
-    email: '',
-    password: '',
-    type: '',
-  },
-  setUserCredentials: (user) => set((state) => ({ ...state, user })),
+  // on load check if there is user data in local storage
+  userData: loadUserDataFromStorage(),
+  setUserData: (userData) =>
+    set((state) => {
+      setUserDataToStorage(userData);
+      return { userData };
+    }),
+  removeUserData: () =>
+    set((state) => {
+      removeUserDataFromStorage();
+      return { userData: null };
+    }),
 }));
 
 export default useStore;
