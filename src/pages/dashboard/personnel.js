08b831314aaa394231bd861fa17ef676a9e18eb0
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-09 16:37:50 -0500

    Adds personnel page

---
+++ src/pages/dashboard/personnel.js
@@ -0,0 +1,13 @@
+import DefaultLayout from '@/components/layouts/DefaultLayout';
+
+export default function Personnel() {
+  return (
+    <DefaultLayout>
+      <div className='bg-gray-300 w-full py-2 text-center'>
+        <h1 className='text-3xl font-semibold'>
+          Welcome to the Enterprise Learner Record Repository&nbsp;
+        </h1>
+      </div>
+    </DefaultLayout>
+  );
+}
