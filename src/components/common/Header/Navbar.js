9c31cbf62d2a4317c0df3bcd7adc965017c195a6
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-09 11:44:22 -0500

    Updates menu option name

--- src/components/common/Header/Navbar.js
+++ src/components/common/Header/Navbar.js
@@ -29,20 +29,20 @@ const nav_options_learner = [
 ];
 
 const nav_options = {
-  manager: nav_options_career_manager,
+  CareerManager: nav_options_career_manager,
   learner: nav_options_learner,
 };
 
 const LoggedInNavbar = ({ userData, logout }) => {
   return (
-    <header className='inline-flex text-white w-full py-2 justify-between items-center'>
-      <div>
+    <header className='flex text-white w-full py-2 items-center justify-between'>
+      <div className='flex justify-self-start'>
         {userData?.type &&
           nav_options[userData.type]?.map((option) => {
             return <NavBtn key={option.name} btn={option} />;
           })}
       </div>
-      <div className='text-right' onClick={logout}>
+      <div className='' onClick={logout}>
         {userData?.type && <div>{userData?.type}</div>}
         {userData?.user?.name && <div>{userData?.user.name}</div>}
       </div>
@@ -52,13 +52,13 @@ const LoggedInNavbar = ({ userData, logout }) => {
 
 const LoggedOutNavbar = () => {
   return (
-    <header className='inline-flex text-white w-full py-2 justify-end items-center'>
+    <div className='inline-flex text-white w-full py-2 justify-end items-center'>
       <Link href='/' passHref>
         <button className='inline-flex items-end px-4 py-2 text-sm font-bold leading-5 text-white transition duration-150 ease-in-out bg-dod-500 border border-transparent rounded-md hover:bg-dod-700 focus:outline-none focus:ring-dod-500 focus:ring-2 ring-offset-1 focus:border-dod-500'>
           login
         </button>
       </Link>
-    </header>
+    </div>
   );
 };
 
@@ -70,10 +70,10 @@ export default function Navbar() {
     router.push('/');
     removeUserData();
   };
-  return (
-    <>
-      {!userData && <LoggedOutNavbar />}
-      {userData && <LoggedInNavbar logout={handleLogout} userData={userData} />}
-    </>
+
+  return userData ? (
+    <LoggedInNavbar userData={userData} logout={handleLogout} key='logged-in' />
+  ) : (
+    <LoggedOutNavbar />
   );
 }
