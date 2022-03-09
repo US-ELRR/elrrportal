da10c941368d442167ee078cfa06b0ac44a33f0d
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-09 11:43:25 -0500

    adds enpoints for dashboard page

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,5 +1,5 @@
-import userData from '@/data/managerUser.json';
+import careerManagerData from '@/data/careerManagerData.json';
 
 export default function handler(req, res) {
-  res.status(200).json(userData);
+  res.status(200).json(careerManagerData);
 }
