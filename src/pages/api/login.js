f6a1ff58dd1ef3bde3e49573320a98925d713f79
schittlur <78377530+schittlur@users.noreply.github.com>
2022-03-09 17:06:03 -0500

    login

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,5 +1,9 @@
 import careerManagerData from '@/data/careerManagerData.json';
+import learnerData from '@/data/learnerData.json';
 
 export default function handler(req, res) {
+  if(req.body.username.toLowerCase().includes('learner')){
+    res.status(200).json(learnerData);
+  }
   res.status(200).json(careerManagerData);
 }
