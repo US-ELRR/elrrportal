fe9c7eb2cc7865a2fc33ecc223fc3b6423a6e5c0
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-09 09:06:34 -0500

    Refactors navbar

--- src/components/common/Header/Navbar.js
+++ src/components/common/Header/Navbar.js
@@ -6,7 +6,7 @@ import NavBtn from './NavBtn';
 const nav_options_career_manager = [
   {
     name: 'Dashboard',
-    path: '/manager/career/dashboard', // will need to be reactive based on the user type
+    path: '/dashboard', // will need to be reactive based on the user type
   },
   {
     name: 'Personnel',
@@ -24,7 +24,7 @@ const nav_options_career_manager = [
 const nav_options_learner = [
   {
     name: 'Dashboard',
-    path: '/learner', // will need to be reactive based on the user type
+    path: '/dashboard', // will need to be reactive based on the user type
   },
 ];
 
@@ -33,14 +33,7 @@ const nav_options = {
   learner: nav_options_learner,
 };
 
-export default function Navbar() {
-  const { userData, removeUserData } = useStore((state) => state);
-  const router = useRouter();
-
-  const logout = () => {
-    router.push('/');
-    removeUserData();
-  };
+const LoggedInNavbar = ({ userData, logout }) => {
   return (
     <header className='inline-flex text-white w-full py-2 justify-between items-center'>
       <div>
@@ -49,14 +42,38 @@ export default function Navbar() {
             return <NavBtn key={option.name} btn={option} />;
           })}
       </div>
-      {!userData?.user?.name && (
-        <Link href='/' passHref>
-          <button className='inline-flex items-center px-4 py-2 mr-2 text-sm font-bold leading-5 text-white transition duration-150 ease-in-out bg-dod-500 border border-transparent rounded-md hover:bg-dod-700 focus:outline-none focus:ring-dod-500 focus:ring-2 ring-offset-1 focus:border-dod-500'>
-            login
-          </button>
-        </Link>
-      )}
-      {userData?.user?.name && <div onClick={logout}>{userData?.user.name}</div>}
+      <div className='text-right' onClick={logout}>
+        {userData?.type && <div>{userData?.type}</div>}
+        {userData?.user?.name && <div>{userData?.user.name}</div>}
+      </div>
     </header>
+  );
+};
+
+const LoggedOutNavbar = () => {
+  return (
+    <header className='inline-flex text-white w-full py-2 justify-end items-center'>
+      <Link href='/' passHref>
+        <button className='inline-flex items-end px-4 py-2 text-sm font-bold leading-5 text-white transition duration-150 ease-in-out bg-dod-500 border border-transparent rounded-md hover:bg-dod-700 focus:outline-none focus:ring-dod-500 focus:ring-2 ring-offset-1 focus:border-dod-500'>
+          login
+        </button>
+      </Link>
+    </header>
+  );
+};
+
+export default function Navbar() {
+  const { userData, removeUserData } = useStore((state) => state);
+  const router = useRouter();
+
+  const handleLogout = () => {
+    router.push('/');
+    removeUserData();
+  };
+  return (
+    <>
+      {!userData && <LoggedOutNavbar />}
+      {userData && <LoggedInNavbar logout={handleLogout} userData={userData} />}
+    </>
   );
 }
