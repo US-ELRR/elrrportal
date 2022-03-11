c0f974312d25e7973927a4b370b98d00f9a6f1a7
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-11 12:56:24 -0500

    adds api's for manager personel page

---
+++ src/pages/api/personnel/[id].js
@@ -0,0 +1,31 @@
+import personnelData from '@/data/personnelData.json';
+
+export default function handler(req, res, next) {
+  // get the personid from the url
+  const { id } = req.query;
+  const {
+    data: { learners },
+  } = personnelData;
+  // console.log(learners);
+
+  // iterate over the array of learners
+  let person = learners.find(
+    (learner) => learner.personnel.person.personid == id
+  );
+
+  // if the person is not found, return an error
+  if (!person) {
+    return res.status(404).send({
+      error: 'Person not found',
+    });
+  }
+
+  // flatten the data for rendering
+  person = {
+    ...person.personnel,
+    courses: person.courses,
+    competencies: person.competencies,
+  };
+
+  return res.status(200).send(person);
+}
