15efe5b5c6028d9737d8036eeca84095f1839c50
rglasgow-deloitte <rglasgow@deloitte.com>
2022-03-16 13:10:00 -0400

    Removes unused code

--- src/components/manager/career/CareerManagerDashboard.js
+++ src/components/manager/career/CareerManagerDashboard.js
@@ -1,35 +1,18 @@
 import Banner from '@/components/manager/common/Banner';
 import EnrolledCourses from '@/components/manager/common/EnrolledCourses';
 import CompletedCompetencies from '@/components/manager/common/CompletedCompetencies';
-
-import { useEffect, useState } from 'react';
-import { unstable_batchedUpdates } from 'react-dom';
 import useStore from '@/store/store';
-import axios from 'axios';
 
 export default function CareerManagerDashboard() {
   const userData = useStore((state) => state.userData);
-  const [enrolledCourses, setEnrolledCourses] = useState([]);
-  const [completedCompetencies, setCompletedCompetencies] = useState([]);
-
-  // on load, get the enrolled courses and competencies
-  useEffect(() => {
-    unstable_batchedUpdates(() => {
-      axios.get('/api/courses').then((res) => {
-        setEnrolledCourses(res.data);
-      });
-      axios.get('/api/competencies').then((res) => {
-        setCompletedCompetencies(res.data);
-      });
-    });
-  }, []);
-
   return (
     <>
       <Banner user={userData?.learner.personnel.person} />
       <div className='flex gap-4 mt-10'>
         <EnrolledCourses courses={userData?.learner.courses} />
-        <CompletedCompetencies completedCompetencies={userData?.learner.competencies} />
+        <CompletedCompetencies
+          completedCompetencies={userData?.learner.competencies}
+        />
       </div>
     </>
   );
