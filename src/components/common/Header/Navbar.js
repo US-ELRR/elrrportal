2c9efcde592c90a3f591e294680cbc96eac1f91a
rglasgow-deloitte <rglasgow@deloitte.com>
2022-04-26 14:53:59 -0400

    updates header to have logout button

--- src/components/common/Header/Navbar.js
+++ src/components/common/Header/Navbar.js
@@ -79,7 +79,7 @@ export default function Navbar({ userData, logout }) {
         )}
         {userData && (
           <button
-            className='inline-flex items-center px-4 py-2 mr-2 text-sm font-bold leading-5 text-white transition duration-75 ease-in-out bg-dod-500 border border-transparent rounded-md hover:bg-dod-700 focus:outline-none focus:ring-dod-500 focus:ring-2 ring-offset-1 focus:border-dod-500'
+            className='inline-flex items-center px-4 py-0.5 text-sm font-bold leading-5 text-white transition duration-75 ease-in-out bg-dod-500 border border-transparent rounded-md hover:bg-dod-700 focus:outline-none focus:ring-dod-500 focus:ring-2 ring-offset-1 focus:border-dod-500'
             onClick={logout}
           >
             Logout
