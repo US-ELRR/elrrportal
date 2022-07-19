51c0e9d5f0708946c53712e773431baad05ca9f7
Peterson <jospeterson@deloitte.com>
2022-07-19 14:33:30 -0700

    system admin without data

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,14 +1,21 @@
 import careerManagerData from '@/data/career_manager/careerManagerData.json';
 import learnerData from '@/data/learner/learnerData.json';
+import systemAdminData from '@/data/systemAdmin/systemAdminData.json';
 import trainingManagerData from '@/data/training_manager/trainingManagerData.json';
 
 export default function handler(req, res) {
   const { username } = req.body;
   if (username.toLowerCase().includes('careermanager')) {
     return res.status(200).json(careerManagerData);
-  } else if (username.toLowerCase().includes('trainingmanager')) {
+  }
+  if (username.toLowerCase().includes('trainingmanager')) {
     return res.status(200).json(trainingManagerData);
   }
-
-  return res.status(200).json(learnerData);
+  if (username.toLowerCase().includes('learner')) {
+    return res.status(200).json(learnerData);
+  }
+  if (username.toLowerCase().includes('systemadmin')) {
+    return res.status(200).json(systemAdminData);
+  }
+  return res.status(401).json()
 }
